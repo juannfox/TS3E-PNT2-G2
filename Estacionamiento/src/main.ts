8d94cc396d760cb9bc3c21d47e11b2695dfeb4f1
Juan Fox <juannfox@hotmail.com>
2023-05-14 14:16:13 -0300

    Re-scaffold con ionic sidemenu

--- Estacionamiento/src/main.ts
+++ Estacionamiento/src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index.js'
-import { createPinia } from 'pinia'
+import router from './router';
+
 import { IonicVue } from '@ionic/vue';
+
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
+
 /* Basic CSS for apps built with Ionic */
 import '@ionic/vue/css/normalize.css';
 import '@ionic/vue/css/structure.css';
 import '@ionic/vue/css/typography.css';
+
 /* Optional CSS utils that can be commented out */
 import '@ionic/vue/css/padding.css';
 import '@ionic/vue/css/float-elements.css';
@@ -16,12 +19,14 @@ import '@ionic/vue/css/text-alignment.css';
 import '@ionic/vue/css/text-transformation.css';
 import '@ionic/vue/css/flex-utils.css';
 import '@ionic/vue/css/display.css';
-import "./assets/main.css";
+
+/* Theme variables */
+import './theme/variables.css';
 
 const app = createApp(App)
-app.use(createPinia())
-app.use(IonicVue)
-app.use(router)
+  .use(IonicVue)
+  .use(router);
+  
 router.isReady().then(() => {
-    app.mount('#app');
+  app.mount('#app');
 });
