8a0d66f3d88bfcd33421f14e5883ce275030e7c9
diegosper <diego.sperber@gmail.com>
2023-05-30 01:03:14 +0200

    authentication login

--- Estacionamiento/src/router/index.js
+++ Estacionamiento/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { useLoginStore } from '@/state/loginStore.js';
 
 const routes = [
   {
@@ -14,19 +15,23 @@ const routes = [
   },
   {
     path: '/mapa',
-    component: () => import('@/views/MapaView.vue')
+    component: () => import('@/views/MapaView.vue'),
+    meta: { RequireAuth: true }
   },
   {
     path: '/miestacionamiento',
-    component: () => import('@/views/DetallesEstacionamiento.vue')
+    component: () => import('@/views/DetallesEstacionamiento.vue'),
+    meta: { RequireAuth: true }
   },
   {
     path: '/usuarios',
-    component: () => import('@/views/UsuariosView.vue')
+    component: () => import('@/views/UsuariosView.vue'),
+    meta: { RequireAuth: true }
   },
   {
     path: '/usuarios/perfil',
-    component: () => import('@/views/DetallesUsuarioView.vue')
+    component: () => import('@/views/DetallesUsuarioView.vue'),
+    meta: { RequireAuth: true }
   }
 
 ]
@@ -35,5 +40,16 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
+
+
+router.beforeEach((to, from, next) => {
+  const store = useLoginStore();
+  if (to.matched.some(r => r.meta.RequireAuth) && !store.loggedIn) {
+    next('/login')  // ir a una ruta que indique error 401
+  } else {
+    next()
+  }
+})
+
 
 export default router
--- Estacionamiento/src/views/DetallesUsuarioView.vue
+++ Estacionamiento/src/views/DetallesUsuarioView.vue
@@ -3,30 +3,34 @@
     <ion-header></ion-header>
 
     <h1>{{ loggedUser.name }}</h1>
-    
+
     <ion-button @click="volverAlHome">Volver al home</ion-button>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonButton } from "@ionic/vue";
+import { IonPage, IonButton, IonHeader } from "@ionic/vue";
 
 import { storeToRefs } from "pinia";
 import { useLoginStore } from "@/state/loginStore.js";
-
-const loginStore = useLoginStore();
-const { loggedIn, loggedUser } = storeToRefs(loginStore);
-const { logout } = loginStore;
 
 export default {
   components: {
     IonButton,
     IonPage,
+    IonHeader,
   },
   methods: {
     volverAlHome() {
       this.$router.push("/");
     },
+  },
+
+  setup() {
+    const loginStore = useLoginStore();
+    const { loggedUser } = storeToRefs(loginStore);
+
+    return { loggedUser };
   },
 };
 </script>
